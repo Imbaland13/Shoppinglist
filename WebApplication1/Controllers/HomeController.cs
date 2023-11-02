1d31bf828f6b9f014a8d5c1f68943ce0700cb56b
Imbaland13 <122046018+Imbaland13@users.noreply.github.com>
2023-11-02 18:13:07 +0100

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult ProductForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult ProductForm(Product product)
+        {
+            Repository.AddResponse(product);
+            return View();
+        }
+        public ViewResult ShoppingList()
+        {
+            return View(Repository.Responses);
+        }
+    }
+}
---
+++ WebApplication1/Models/Repository.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+	public static class Repository
+	{
+		private static List<Product> responses = new();
+		public static IEnumerable<Product> Responses => responses;
+		public static void AddResponse(Product response)
+		{
+			responses.Add(response);
+		}
+	}
+}
